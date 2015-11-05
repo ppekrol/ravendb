2fd80ec9780deff94d6180c250833c908dc5164f
Lars Corneliussen <lc@faktum.co>
2015-11-05 11:57:22 +0100

    Fixed logging of reduce keys

--- Raven.Database/Indexing/ReducingExecuter.cs
+++ Raven.Database/Indexing/ReducingExecuter.cs
@@ -69,7 +69,7 @@ namespace Raven.Database.Indexing
 				if (singleStepReduceKeys.Count > 0)
 				{
                     if ( Log.IsDebugEnabled )
-                        Log.Debug("SingleStep reduce for keys: {0}", singleStepReduceKeys.Select(x => x + ","));
+                        Log.Debug("SingleStep reduce for keys: {0}", String.Join(", ", singleStepReduceKeys));
                     
 					var singleStepStats = SingleStepReduce(indexToWorkOn, singleStepReduceKeys, viewGenerator, itemsToDelete, token);
 
@@ -79,7 +79,7 @@ namespace Raven.Database.Indexing
 				if (multiStepsReduceKeys.Count > 0)
 				{
                     if ( Log.IsDebugEnabled )
-                        Log.Debug("MultiStep reduce for keys: {0}", multiStepsReduceKeys.Select(x => x + ","));
+                        Log.Debug("MultiStep reduce for keys: {0}", String.Join(", ", multiStepsReduceKeys));
 
 					var multiStepStats = MultiStepReduce(indexToWorkOn, multiStepsReduceKeys, viewGenerator, itemsToDelete, token);
 
