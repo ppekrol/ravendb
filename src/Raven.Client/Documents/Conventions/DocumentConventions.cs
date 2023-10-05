76c6dbb1b2799abc45e93682af4b525c696e9c48
Pawel Pekrol <ppekrol@ravendb.net>
2023-10-05 12:48:06 +0200

    RavenDB-21528 added support for brotli compression

--- src/Raven.Client/Documents/Conventions/DocumentConventions.cs
+++ src/Raven.Client/Documents/Conventions/DocumentConventions.cs
@@ -257,9 +257,9 @@ namespace Raven.Client.Documents.Conventions
 
             TransformTypeCollectionNameToDocumentIdPrefix = DefaultTransformCollectionNameToDocumentIdPrefix;
             FindCollectionName = DefaultGetCollectionName;
-            
+
             ShouldApplyPropertyNameConverter = DefaultShouldApplyPropertyNameConverter;
-            
+
             FindPropertyNameForIndex = DefaultFindPropertyNameForIndex;
             FindPropertyNameForDynamicIndex = DefaultFindPropertyNameForDynamicIndex;
 
@@ -303,6 +303,8 @@ namespace Raven.Client.Documents.Conventions
             _createHttpClient = handler => new HttpClient(handler);
 
             _disposeCertificate = true;
+
+            _httpCompressionAlgorithm = HttpCompressionAlgorithm.Gzip;
         }
 
         private bool _frozen;
@@ -350,6 +352,7 @@ namespace Raven.Client.Documents.Conventions
         private Size _maxHttpCacheSize;
         private bool? _useHttpDecompression;
         private bool? _useHttpCompression;
+        private HttpCompressionAlgorithm _httpCompressionAlgorithm;
         private Func<MemberInfo, string> _propertyNameConverter;
         private Func<Type, bool> _typeIsKnownServerSide = _ => false;
         private Func<MemberInfo, bool> _shouldApplyPropertyNameConverter;
@@ -389,7 +392,7 @@ namespace Raven.Client.Documents.Conventions
                 _shouldApplyPropertyNameConverter = value;
             }
         }
-        
+
         public Size MaxContextSizeToKeep
         {
             get => _maxContextSizeToKeep;
@@ -591,6 +594,16 @@ namespace Raven.Client.Documents.Conventions
             }
         }
 
+        public HttpCompressionAlgorithm HttpCompressionAlgorithm
+        {
+            get => _httpCompressionAlgorithm;
+            set
+            {
+                AssertNotFrozen();
+                _httpCompressionAlgorithm = value;
+            }
+        }
+
         /// <summary>
         /// Use gzip compression when sending content of HTTP request
         /// </summary>
@@ -607,7 +620,7 @@ namespace Raven.Client.Documents.Conventions
         internal bool HasExplicitlySetDecompressionUsage => _useHttpDecompression.HasValue;
 
         /// <summary>
-        /// Can accept compressed HTTP response content and will use gzip/deflate decompression methods
+        /// Can accept compressed HTTP response content and will use brotli/gzip/deflate decompression methods
         /// </summary>
         public bool UseHttpDecompression
         {
@@ -1059,7 +1072,7 @@ namespace Raven.Client.Documents.Conventions
                 CachedDefaultTypeCollectionNames.Put(t, null);
                 return null;
             }
-            
+
             // we want to reject queries and other operations on abstract types, because you usually
             // want to use them for polymorphic queries, and that require the conventions to be
             // applied properly, so we reject the behavior and hint to the user explicitly
@@ -1303,7 +1316,7 @@ namespace Raven.Client.Documents.Conventions
                 _identityPartsSeparator = configuration.IdentityPartsSeparator ?? _originalConfiguration.IdentityPartsSeparator ?? _identityPartsSeparator;
             }
         }
-        
+
         public static bool DefaultShouldApplyPropertyNameConverter(MemberInfo member) => (member.DeclaringType?.Namespace?.StartsWith("System") == true ||
                                                                                             member.DeclaringType?.Namespace?.StartsWith("Microsoft") == true) == false;
         public static string DefaultTransformCollectionNameToDocumentIdPrefix(string collectionName)
@@ -1461,7 +1474,7 @@ namespace Raven.Client.Documents.Conventions
 
             if (objValueFunc != null)
                 return objValueFunc(fieldName, value, forRange, out objValue);
-            
+
             objValue = null;
             return false;
         }
