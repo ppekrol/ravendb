4a93125c96c67030fcc605564f9fb2379cde416e
Pawel Pekrol <ppekrol@ravendb.net>
2023-10-05 13:14:09 +0200

    RavenDB-21528 temp

--- src/Raven.Client/Documents/Conventions/DocumentConventions.cs
+++ src/Raven.Client/Documents/Conventions/DocumentConventions.cs
@@ -289,6 +289,9 @@ namespace Raven.Client.Documents.Conventions
             _disposeCertificate = true;
 
             _httpCompressionAlgorithm = HttpCompressionAlgorithm.Gzip;
+#if FEATURE_BROTLI_SUPPORT
+            _httpCompressionAlgorithm = HttpCompressionAlgorithm.Brotli; // temp
+#endif
         }
 
         private bool _frozen;
