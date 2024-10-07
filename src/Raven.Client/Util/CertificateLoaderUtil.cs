8bca120e8f46172c2c9f00c110534b61b81ba4cd
Paweł Pekról <ppekrol@ravendb.net>
2024-10-07 23:18:04 +0200

    RavenDB-22985 fixing warnings

--- src/Raven.Client/Util/CertificateLoaderUtil.cs
+++ src/Raven.Client/Util/CertificateLoaderUtil.cs
@@ -39,12 +39,20 @@ internal static class CertificateLoaderUtil
 
     public static X509Certificate2 CreateCertificate(byte[] rawData, string password = null, X509KeyStorageFlags? flags = null)
     {
+#if NET9_0_OR_GREATER
+        return CreateCertificate(f => X509CertificateLoader.LoadPkcs12(rawData, password, f), flags);
+#else
         return CreateCertificate(f => new X509Certificate2(rawData, password, f), flags);
+#endif
     }
 
     internal static X509Certificate2 CreateCertificate(string fileName, string password = null, X509KeyStorageFlags? flags = null)
     {
+#if NET9_0_OR_GREATER
+        return CreateCertificate(f => X509CertificateLoader.LoadPkcs12FromFile(fileName, password, f), flags);
+#else
         return CreateCertificate(f => new X509Certificate2(fileName, password, f), flags);
+#endif
     }
 
     private static X509Certificate2 CreateCertificate(Func<X509KeyStorageFlags, X509Certificate2> creator, X509KeyStorageFlags? flag)
@@ -58,13 +66,13 @@ internal static class CertificateLoaderUtil
         {
             certificate = creator(f);
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             exception = e;
             f = AddMachineKeySet(flag);
             certificate = creator(f);
         }
-        
+
         LogIfNeeded(nameof(CreateCertificate), f, exception);
 
         CertificateCleaner.RegisterForDisposalDuringFinalization(certificate);
@@ -90,10 +98,10 @@ internal static class CertificateLoaderUtil
 #endif
             X509KeyStorageFlags.UserKeySet |
             X509KeyStorageFlags.MachineKeySet;
-        
+
         Debug.Assert(flags.HasValue == false || (flags.Value & keyStorageFlags) == 0);
     }
-    
+
     private static void LogIfNeeded(string method, X509KeyStorageFlags flags, Exception exception)
     {
         if (FirstTime)
