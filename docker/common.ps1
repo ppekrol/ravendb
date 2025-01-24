85eeedd26fc1a40275e98a25859197c0480b09d3
Paweł Pekról <ppekrol@ravendb.net>
2025-01-24 21:53:28 +0100

    RavenDB-7070 after merge fixes

--- docker/common.ps1
+++ docker/common.ps1
@@ -35,7 +35,7 @@ function GetUbuntuImageTags($repo, $version, $arch) {
     switch ($arch) {
         "x64" { 
             return @(
-                "$($repo):ubuntu-latest",
+#                "$($repo):ubuntu-latest",
                 "$($repo):7.1-ubuntu-latest",
                 "$($repo):$($version)-ubuntu.24.04-x64"
             )
@@ -43,7 +43,7 @@ function GetUbuntuImageTags($repo, $version, $arch) {
         }
         "arm32v7" {
             return @(
-                "$($repo):ubuntu-arm32v7-latest",
+#                "$($repo):ubuntu-arm32v7-latest",
                 "$($repo):7.1-ubuntu-arm32v7-latest",
                 "$($repo):$($version)-ubuntu.24.04-arm32v7"
             )
@@ -51,7 +51,7 @@ function GetUbuntuImageTags($repo, $version, $arch) {
         }
         "arm64v8" {
             return @(
-                "$($repo):ubuntu-arm64v8-latest",
+#                "$($repo):ubuntu-arm64v8-latest",
                 "$($repo):7.1-ubuntu-arm64v8-latest",
                 "$($repo):$($version)-ubuntu.24.04-arm64v8"
                 )
@@ -68,19 +68,15 @@ function GetWindowsImageTags($repo, $version, $WinVer) {
     switch ($winver) {
         "1809" {
             return @(
-                "$($repo):windows-1809-latest",
-                "$($repo):$($version)-windows-1809",
 #                "$($repo):windows-1809-latest",
-                "$($repo):7.1-windows-1809-latest"
+                "$($repo):$($version)-windows-1809",
             )
             break;
         }
         "ltsc2022" {
              return @(
-                "$($repo):windows-ltsc2022-latest",
-                "$($repo):$($version)-windows-ltsc2022",
 #                "$($repo):windows-ltsc2022-latest",
-                "$($repo):7.1-windows-ltsc2022-latest"
+                "$($repo):$($version)-windows-ltsc2022"
             )
             break;
         }
@@ -97,7 +93,7 @@ function GetManifestTags {
     )
 
     return @(
-        "${repo}:latest",
+#        "${repo}:latest",
         "${repo}:7.1-latest"
     )
 }
