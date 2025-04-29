c4dd789571a796c0000645c5ed7929c1dbeb0bc7
aviv <aviv@ravendb.net>
2025-04-29 17:29:56 +0300

    RavenDB-23942 : add tests for edit/delete/toggle-state

--- test/FastTests/GenAi/GenAiBasics.cs
+++ test/FastTests/GenAi/GenAiBasics.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Raven.Client.Documents.Operations.AI;
 using Raven.Client.Documents.Operations.ConnectionStrings;
+using Raven.Client.Documents.Operations.ETL;
 using Raven.Client.Documents.Operations.OngoingTasks;
 using Tests.Infrastructure;
 using Xunit;
@@ -197,6 +198,206 @@ for(const comment of this.Comments)
         Assert.Equal(configuration.Update, genAiTaskInfo.Configuration.Update);
         Assert.Equal(configuration.AiConnectorType, genAiTaskInfo.Configuration.AiConnectorType);
         Assert.Equal(configuration.GenAiTransformation.Script, genAiTaskInfo.Configuration.GenAiTransformation.Script);
+    }
+
+    [RavenFact(RavenTestCategory.Ai)]
+    public async Task CanEditGenAiTask()
+    {
+        const string connectionStrName = "ollama-local-deepseek-r1";
+
+        using var store = GetDocumentStore();
+        store.Maintenance.Send(new PutConnectionStringOperation<AiConnectionString>(new AiConnectionString
+        {
+            Name = connectionStrName,
+            Identifier = connectionStrName,
+            OllamaSettings = new OllamaSettings
+            {
+                Uri = "http://127.0.0.1:11434/",
+                Model = "deepseek-r1:1.5b"
+            }
+        }));
+
+
+        var configuration = new GenAiConfiguration
+        {
+            Name = "Check blog comments spam",
+            ConnectionStringName = connectionStrName,
+            Prompt = "Check if the following blog post comment is spam or not",
+            Collection = "Posts",
+            SampleObject = JsonConvert.SerializeObject(new
+            {
+                Blocked = true,
+                Reason = "Concise reason for why this comment was marked as spam or ham"
+            }),
+            Update = @"    
+const idx = this.Comments.findIndex(c => c.Id == $input.Id);  
+if($output.Blocked)
+{
+    this.Comments.splice(idx, 1); // remove
+}
+else 
+{
+    this.Comments[idx].AiHash = $aiHash; // remember this decision
+}",
+            GenAiTransformation = new GenAiTransformation
+            {
+                Script = @"
+for(const comment of this.Comments)
+{
+    context({Text: comment.Text, Author: comment.Author, Id: comment.Id}, comment.AiHash);
+}
+"
+            }
+        };
+
+        store.Maintenance.Send(new AddGenAiOperation(configuration));
+
+        var op = new GetOngoingTaskInfoOperation(configuration.Name, OngoingTaskType.GenAi);
+        var taskInfo = await store.Maintenance.SendAsync(op);
+        var taskId = taskInfo.TaskId;
+
+        var newUpdateScript = @"const idx = this.Comments.findIndex(c => c.Id == $input.Id);
+this.Comments[idx].LegitComment = $output.Blocked == false;
+";
+        configuration.Update = newUpdateScript;
+
+        store.Maintenance.Send(new UpdateEtlOperation<AiConnectionString>(taskId, configuration));
+
+        op = new GetOngoingTaskInfoOperation(configuration.Name, OngoingTaskType.GenAi);
+        taskInfo = await store.Maintenance.SendAsync(op);
+
+        var genAiTaskInfo = taskInfo as Raven.Client.Documents.Operations.OngoingTasks.GenAi;
+        Assert.NotNull(genAiTaskInfo);
+        Assert.Equal(newUpdateScript, genAiTaskInfo.Configuration.Update);
+    }
+
+    [RavenFact(RavenTestCategory.Ai)]
+    public async Task CanDeleteGenAiTask()
+    {
+        const string connectionStrName = "ollama-local-deepseek-r1";
+
+        using var store = GetDocumentStore();
+        store.Maintenance.Send(new PutConnectionStringOperation<AiConnectionString>(new AiConnectionString
+        {
+            Name = connectionStrName,
+            Identifier = connectionStrName,
+            OllamaSettings = new OllamaSettings
+            {
+                Uri = "http://127.0.0.1:11434/",
+                Model = "deepseek-r1:1.5b"
+            }
+        }));
+
+
+        var configuration = new GenAiConfiguration
+        {
+            Name = "Check blog comments spam",
+            ConnectionStringName = connectionStrName,
+            Prompt = "Check if the following blog post comment is spam or not",
+            Collection = "Posts",
+            SampleObject = JsonConvert.SerializeObject(new
+            {
+                Blocked = true,
+                Reason = "Concise reason for why this comment was marked as spam or ham"
+            }),
+            Update = @"    
+const idx = this.Comments.findIndex(c => c.Id == $input.Id);  
+if($output.Blocked)
+{
+    this.Comments.splice(idx, 1); // remove
+}
+else 
+{
+    this.Comments[idx].AiHash = $aiHash; // remember this decision
+}",
+            GenAiTransformation = new GenAiTransformation
+            {
+                Script = @"
+for(const comment of this.Comments)
+{
+    context({Text: comment.Text, Author: comment.Author, Id: comment.Id}, comment.AiHash);
+}
+"
+            }
+        };
+
+        store.Maintenance.Send(new AddGenAiOperation(configuration));
+
+        var taskInfo = await store.Maintenance.SendAsync(new GetOngoingTaskInfoOperation(configuration.Name, OngoingTaskType.GenAi));
+        Assert.NotNull(taskInfo);
+        var taskId = taskInfo.TaskId;
+
+        store.Maintenance.Send(new DeleteOngoingTaskOperation(taskId, OngoingTaskType.GenAi));
+        taskInfo = await store.Maintenance.SendAsync(new GetOngoingTaskInfoOperation(configuration.Name, OngoingTaskType.GenAi));
+        Assert.Null(taskInfo);
+    }
+
+    [RavenFact(RavenTestCategory.Ai)]
+    public async Task CanToggleGenAiTaskState()
+    {
+        const string connectionStrName = "ollama-local-deepseek-r1";
+
+        using var store = GetDocumentStore();
+        store.Maintenance.Send(new PutConnectionStringOperation<AiConnectionString>(new AiConnectionString
+        {
+            Name = connectionStrName,
+            Identifier = connectionStrName,
+            OllamaSettings = new OllamaSettings
+            {
+                Uri = "http://127.0.0.1:11434/",
+                Model = "deepseek-r1:1.5b"
+            }
+        }));
+
+
+        var configuration = new GenAiConfiguration
+        {
+            Name = "Check blog comments spam",
+            ConnectionStringName = connectionStrName,
+            Prompt = "Check if the following blog post comment is spam or not",
+            Collection = "Posts",
+            SampleObject = JsonConvert.SerializeObject(new
+            {
+                Blocked = true,
+                Reason = "Concise reason for why this comment was marked as spam or ham"
+            }),
+            Update = @"    
+const idx = this.Comments.findIndex(c => c.Id == $input.Id);  
+if($output.Blocked)
+{
+    this.Comments.splice(idx, 1); // remove
+}
+else 
+{
+    this.Comments[idx].AiHash = $aiHash; // remember this decision
+}",
+            GenAiTransformation = new GenAiTransformation
+            {
+                Script = @"
+for(const comment of this.Comments)
+{
+    context({Text: comment.Text, Author: comment.Author, Id: comment.Id}, comment.AiHash);
+}
+"
+            }
+        };
+
+        store.Maintenance.Send(new AddGenAiOperation(configuration));
+
+        var taskInfo = await store.Maintenance.SendAsync(new GetOngoingTaskInfoOperation(configuration.Name, OngoingTaskType.GenAi));
+        Assert.Equal(OngoingTaskState.Enabled, taskInfo.TaskState);
+        var taskId = taskInfo.TaskId;
+
+        // disable task
+        await store.Maintenance.SendAsync(new ToggleOngoingTaskStateOperation(taskId, OngoingTaskType.GenAi, disable: true));
+
+        taskInfo = await store.Maintenance.SendAsync(new GetOngoingTaskInfoOperation(configuration.Name, OngoingTaskType.GenAi));
+        Assert.Equal(OngoingTaskState.Disabled, taskInfo.TaskState);
+
+        // re-enable task
+        await store.Maintenance.SendAsync(new ToggleOngoingTaskStateOperation(taskId, OngoingTaskType.GenAi, disable: false));
+        taskInfo = await store.Maintenance.SendAsync(new GetOngoingTaskInfoOperation(configuration.Name, OngoingTaskType.GenAi));
+        Assert.Equal(OngoingTaskState.Enabled, taskInfo.TaskState);
     }
 
     internal record Comment(string Text, string Author)
