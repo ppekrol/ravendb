9e5bf1670da15daddbddcae8110b2a28243a7402
Damian Olszewski <damian@ravendb.net>
2025-05-05 13:56:47 +0200

    RavenDB-23942 Improve test results

--- src/Raven.Studio/typescript/components/pages/database/tasks/ongoingTasks/editTasks/editGenAiTask/partials/EditGenAiTaskTestResults.tsx
+++ src/Raven.Studio/typescript/components/pages/database/tasks/ongoingTasks/editTasks/editGenAiTask/partials/EditGenAiTaskTestResults.tsx
@@ -1,6 +1,9 @@
 import { useAppSelector } from "components/store";
 import { editGenAiTaskSelectors } from "../store/editGenAiTaskSlice";
 import AceEditor from "components/common/AceEditor";
+import aceDiff from "common/helpers/text/aceDiff";
+import ReactAce from "react-ace/lib/ace";
+import { useRef, useEffect } from "react";
 
 export default function EditGenAiTaskTestResults() {
     const testStage = useAppSelector(editGenAiTaskSelectors.testStage);
@@ -25,9 +28,40 @@ export default function EditGenAiTaskTestResults() {
                     ))}
                 </div>
             )}
-            {testStage === "ApplyUpdateScript" && updateScriptTestResult && (
-                <AceEditor mode="json" value={updateScriptTestResult} readOnly />
-            )}
+            {testStage === "ApplyUpdateScript" && updateScriptTestResult && <UpdateScriptResult />}
+        </div>
+    );
+}
+
+function UpdateScriptResult() {
+    const oldDoc = ""; // TODO
+    const newDoc = useAppSelector(editGenAiTaskSelectors.updateScriptTestResult);
+
+    const oldDocRef = useRef<ReactAce>(null);
+    const newDocRef = useRef<ReactAce>(null);
+
+    useEffect(() => {
+        if (!oldDocRef.current || !newDocRef.current) {
+            return;
+        }
+
+        // We have different ace versions, lets just use 'any' here instead of editing aceDiff class
+        const aceDiffC = new aceDiff(oldDocRef.current.editor as any, newDocRef.current.editor as any, false);
+        aceDiffC.refresh(false);
+
+        return () => {
+            aceDiffC.destroy();
+        };
+    }, [oldDoc, newDoc]);
+
+    return (
+        <div className="vstack gap-2">
+            <div>
+                <AceEditor aceRef={oldDocRef} value={oldDoc} mode="json" height="100px" />
+            </div>
+            <div>
+                <AceEditor aceRef={newDocRef} value={newDoc} mode="json" height="100px" />
+            </div>
         </div>
     );
 }
